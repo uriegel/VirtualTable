281e07119cd57a35e5ed27a7ec3de678e0e410b2
Uwe <uriegel@hotmail.de>
2021-04-20 09:18:20 +0200

    TODO

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ table.setColumns([{
     }]
 )
 
-// TODO: Themes with different font sizes
 // TODO: Adaptable column widthes
 // TODO: Sortable: highlighted, asc, desc
 // TODO: SubColumn, sortable
