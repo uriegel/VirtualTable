8908aa476c3eb153a499cca02b3b7a8e503a061a
uriegel <uriegel@hotmail.de>
2021-05-11 16:18:51 +0200

    test url vtc, disable cache, restrictor display none

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use warp::Filter;
+extern crate chrono;
+
+use chrono::Utc;
+use warp::{Filter, Reply, fs::File, http::HeaderValue, hyper::{Body, HeaderMap, Response}};
 
 #[tokio::main]
 async fn main() {
     let port = 9865;
-    println!("Running test server on http://localhost:{}", port);
+    println!("Running test server on http://localhost:{}/vtc", port);
 
-    let route1 = warp::path!("hello" / String)
-    .map(|name| {
-        format!("Hello, {}!", name)
-    });
+    fn add_headers(reply: File)->Response<Body> {
+        let mut header_map = HeaderMap::new();
+        let now = Utc::now();
+        let now_str = now.format("%a, %d %h %Y %T GMT").to_string();
+        header_map.insert("Expires", HeaderValue::from_str(now_str.as_str()).unwrap());
+        header_map.insert("Server", HeaderValue::from_str("Mein Server").unwrap());
 
-    //let route1 = warp::path("web").and(warp::fs::dir("/home/uwe/Projekte/WebComponents"));
-    let route2 = warp::fs::dir("/home/uwe/Projekte/VirtualTableComponent");
-    let routes = route1.or(route2);
+        let mut res = reply.into_response();
+        let headers = res.headers_mut();
+        headers.extend(header_map);
+        res
+    }
+
+    let route = 
+        warp::path("vtc")
+        .and(warp::fs::dir("."))
+        .map(add_headers);
 
     //warp::serve(warp::fs::dir("/home/uwe/Projekte/VirtualTableComponent")
-    warp::serve(routes)
+    warp::serve(route)
         .run(([127, 0, 0, 1], port))
         .await;        
 }
